76af0d9e6ff19e1c5386ae69c4d7e949f5e7e1e2
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-07-20 14:32:04 +0200

    refactor Product and Products component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,21 @@
+import { useState } from 'react'
 import styles from './Product.module.scss'
 import clsx from 'clsx'
 import Button from '../Button/Button'
 import PropTypes from 'prop-types'
 
-const Product = ({ title, basePrice }) => {
+const Product = ({ title, basePrice, colors, sizes, name }) => {
+	const [currentColor, setCurrentColor] = useState(colors[0])
+	const [currentSize, setCurrentSize] = useState(sizes[0].name)
+  console.log(currentSize);
+
 	return (
 		<article className={styles.product}>
 			<div className={styles.imageContainer}>
 				<img
 					className={styles.image}
-					alt='Kodilla shirt'
-					src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`}
+					alt={`${name} shirt ${currentColor}`}
+					src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`}
 				/>
 			</div>
 			<div>
@@ -22,34 +27,31 @@ const Product = ({ title, basePrice }) => {
 					<div className={styles.sizes}>
 						<h3 className={styles.optionLabel}>Sizes</h3>
 						<ul className={styles.choices}>
-							<li>
-								<button type='button' className={styles.active}>
-									S
-								</button>
-							</li>
-							<li>
-								<button type='button'>M</button>
-							</li>
-							<li>
-								<button type='button'>L</button>
-							</li>
-							<li>
-								<button type='button'>XL</button>
-							</li>
+							{sizes.map((size, i) => {
+								return (
+									<li key={i}>
+										<button type='button' className={clsx(currentSize === size.name && styles.active)}>
+											{size.name}
+										</button>
+									</li>
+								)
+							})}
 						</ul>
 					</div>
 					<div className={styles.colors}>
 						<h3 className={styles.optionLabel}>Colors</h3>
 						<ul className={styles.choices}>
-							<li>
-								<button type='button' className={clsx(styles.colorBlack, styles.active)} />
-							</li>
-							<li>
-								<button type='button' className={clsx(styles.colorRed)} />
-							</li>
-							<li>
-								<button type='button' className={clsx(styles.colorWhite)} />
-							</li>
+							{colors.map((color, i) => {
+								const col = color.charAt(0).toUpperCase() + color.slice(1).toLowerCase()
+								const colorClassName = `color${col}`
+								return (
+									<li key={i}>
+										<button
+											type='button'
+											className={clsx(styles[colorClassName], currentColor === color && styles.active)}></button>
+									</li>
+								)
+							})}
 						</ul>
 					</div>
 					<Button className={styles.button}>
@@ -61,8 +63,11 @@ const Product = ({ title, basePrice }) => {
 	)
 }
 Product.propTypes = {
-	title: PropTypes.string,
-	basePrice: PropTypes.number,
+	title: PropTypes.string.isRequired,
+	basePrice: PropTypes.number.isRequired,
+	colors: PropTypes.array.isRequired,
+	sizes: PropTypes.array.isRequired,
+	name: PropTypes.string.isRequired,
 }
 
 export default Product
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -5,10 +5,11 @@ import Product from '../Product/Product'
 const Products = () => {
 	const [products] = useState(productsData)
 
+
 	return (
 		<section>
 			{products.map((shirt, i) => {
-				return <Product {...products[i]} key={shirt.id} />
+				return <Product {...products[i]} key={shirt.id}/>
 			})}
 		</section>
 	)
